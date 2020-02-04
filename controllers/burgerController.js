d99814b76934689f736d0d5dc0f0877898c48a7f
Cassidy Fortner <cassidy.fortner@gmail.com>
2020-02-03 23:45:51 -0600

    pushing to ask questions tomorrow

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -7,21 +7,25 @@ module.exports = function(app){
 
     //gets existing burgers to be eaten
     app.get("/", (req, res) => {
-        Burger.findAll()
+        Burger.findAll({})
         .then(data => {
             console.log(data);
-            res.render("index", {burgers: data})
+            res.render("index", {burgers: data});
         })
         .catch(err => console.log(err));
     
     });
-    
+     
+    app.get("/api/burgers", (req, res) => {
+        Burger.findAll({}).then(data => res.json(data));
+    });
     //add burgers to be eaten
     app.post("/api/burgers", (req, res) => {
         console.log(req.body);
         
         Burger.create({
-            burger_name: req.body.burger_name
+            burger_name: req.body.burger_name,
+            
         })
             .then(data => {
                 res.json({id: data.id});
@@ -29,7 +33,6 @@ module.exports = function(app){
             })
             .catch(err => console.log(err));
     
-        res.status(200).end();
        
     });
 }
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,16 +1,17 @@
 $( ()=> {
-    $(".create-form").on("submit", (event) => {
+    $(".add-burger").on("submit", (event) => {
         event.preventDefault();
 
         let newBurger = {
             burger_name: $("#burger").val().trim(),
+            devoured: false
         };
 
         $.ajax("/api/burgers", {
             type: "POST",
             data: newBurger
         }).then( () => {
-            console.log(newBurger);
+            
            
             console.log("Did it work?");
             location.reload();
